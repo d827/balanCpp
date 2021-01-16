3a51509b5520422bf7d2012e0e816e27aa9aba86
DP <dorpetzold@gmail.com>
2021-01-15 21:09:09 -0500

    trying to get fltk to work

--- main.cpp
+++ main.cpp
@@ -2,15 +2,25 @@
 #include <stdio.h>
 #include <string>
 #include <vector>
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
 
 using namespace std;
 
-int main(){
-	bool running = TRUE;
+int main(int argc, char **argv){
+	Fl_Window *window = new Fl_Window(300,180);
+	Fl_Box *box = new Fl_Box(20,40,260,100, "Hello,World!");
+	box->box(FL_UP_BOX);
+	box->labelsize(36);
+	box->labelfont(FL_BOLD+FL_ITALIC);
+	box->labeltype(FL_SHADOW_LABEL);
+	window->end();
+	window->show(argc, argv);
+/*	bool running = FALSE;
 	while(running){
 
 
-	}
-
-	return 0;
+	}*/
+	return Fl::run();
 }
--- makefile
+++ makefile
@@ -1,10 +1,13 @@
 CC      = g++
-FLAGS   = -g
+FLAGS   = -g -std=c++11 '~/../../usr/local/bin/fltk-config --cxxflags --useimages'
+CXXFLAGS += -I/usr/local/include
+LDFLAGS += -L/usr/local/lib
+LIBS = '~/../../usr/local/bin/fltk-config --ldflags' -lfltk -lftlk_images -ljpeg -llstdc++ -lXfixes -lXext -lpthread -ldl -lm -lX11
 
 all: exe
 
 exe: funcs.o
-	$(CC) $(FLAGS) funcs.o -o exe
+	$(CC) $(FLAGS) $(CXXFLAGS) $(LDFLAGS) funcs.o main.cpp -o exe $(LIBS)
 
 funcs.o: funcs.cpp funcs.h
 	$(CC) $(FLAGS) -c funcs.cpp -o funcs.o
